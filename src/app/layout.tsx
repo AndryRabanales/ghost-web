adadccde798ec1657f5a42cf27bb7fb525b5796e
andry_halo2@hotmail.com <andry_halo2@hotmail.com>
2025-10-16 23:59:39 -0600

    qeweqw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,10 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "GhostMessages",
+  title: "GhostMessage",
   // --- 👇 AÑADE ESTO ---
   icons: {
-    icon: '/icon.png', // Asegúrate de que tu PNG se llame icon.png y esté en src/app/
+    icon: '/icono.png', // Asegúrate de que tu PNG se llame icon.png y esté en src/app/
     // apple: '/apple-icon.png', // Opcional para Apple si tienes otro icono
   },
   // --- 👆 HASTA AQUÍ ---
